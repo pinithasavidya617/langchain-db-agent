20be86c51c320d880ac51687fde4d2d39a2f9e35
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-18 15:52:13 +0530

    Add get all users and delete for user routes

--- user_repository.py
+++ user_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_models import UserCreate
@@ -18,4 +19,28 @@ class UserRepository:
         await self.db.refresh(user)
         return user
 
-    
+    async def get_user_by_email(self, email: str) -> User | None:
+        query = select(User).where(User.email == email)
+        result = await self.db.execute(query)
+        return result.scalars().first()
+
+    async def get_all_users(self, skip: int = 0, limit: int = 20) -> list[User]:
+        query = select(User).offset(skip).limit(limit)
+        result = await self.db.execute(query)
+
+        return result.scalars().all()
+
+    async def get_user_by_id(self, id: int) -> User | None:
+         query = select(User).where(User.id == id)
+         result = await self.db.execute(query)
+         return result.scalars().first()
+
+    async def delete_user(self, user_id: int):
+        user = await self.get_user_by_id(user_id)
+
+        if not user:
+            return "User does not exists"
+
+        await self.db.delete(user)
+        await self.db.commit()
+        return "User deleted successfully"
--- user_routes.py
+++ user_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
@@ -12,5 +12,24 @@ router = APIRouter(prefix="/users", tags=["Users"])
 async def create_user(user_create : UserCreate, db: AsyncSession = Depends(get_db)):
 
     user_repo = UserRepository(db)
+    existing_user = await  user_repo.get_user_by_email(user_create.email)
+    if existing_user:
+        return HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User with this email already exists")
     user = await user_repo.create(user_create)
     return user
+
+@router.delete("/{user_id}")
+async def delete_user( user_id:int, db: AsyncSession = Depends(get_db)):
+    user_repo = UserRepository(db)
+    user = await user_repo.delete_user(user_id)
+    return user
+
+@router.get("/")
+async def get_all_users(
+        skip :int = 0,
+        limit:int = 10,
+        db: AsyncSession = Depends(get_db)
+):
+    user_repo = UserRepository(db)
+    users = await user_repo.get_all_users(skip=skip, limit=limit)
+    return users
